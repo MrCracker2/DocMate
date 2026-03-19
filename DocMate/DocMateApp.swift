12a77008d4a93a3044abb14d6067a552ba42993f
MrCracker2 <kumarshashwat888@gmail.com>
2026-03-19 22:08:43 +0530

    Initial Commit

---
+++ DocMate/DocMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocMateApp.swift
+//  DocMate
+//
+//  Created by Shashwat kumar on 19/03/26.
+//
+
+import SwiftUI
+
+@main
+struct DocMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
