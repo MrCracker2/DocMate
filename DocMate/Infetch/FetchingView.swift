e325fd08c21c4cdc3d65c26d7b8d15b231d6f97b
MrCracker2 <kumarshashwat888@gmail.com>
2026-03-20 13:57:58 +0530

    Imp

---
+++ DocMate/Infetch/FetchingView.swift
@@ -0,0 +1,24 @@
+//
+//  FetchingView.swift
+//  DocMate
+//
+//  Created by Shashwat kumar on 20/03/26.
+//
+
+import SwiftUI
+
+struct FetchingView: View {
+    var body: some View {
+        VStack(spacing: 16) {
+            ProgressView()
+            
+            Text("Fetching Document from Inbox")
+                .font(.subheadline)
+                .foregroundColor(.gray)
+        }
+    }
+}
+
+#Preview {
+    FetchingView()
+}
